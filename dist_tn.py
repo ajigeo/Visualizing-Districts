9fe5db4bf40248097fe802f410e09a2185648437
aji <46032638+ajigeo@users.noreply.github.com>
2019-03-04 01:04:31 +0530

    Fixed all the districts & can plot all of em

--- dist_tn.py
+++ dist_tn.py
@@ -4,7 +4,7 @@ import re
 import numpy as np                                                               
 import matplotlib.pyplot as plt
 
-list_of_dists = ['erode','kancheepuram','kanyakumari','karur','krishnagiri']#['ariyalur','chennai','coimbatore','cuddalore','dharmapuri','dindigul']
+list_of_dists = ['madurai','virudhunagar','ramanathapuram','sivaganga','ariyalur','chennai','theni','dindigul','thoothukudi']
 root = []
 
 #dist_1 = str(raw_input("Enter The district 1:"))
@@ -28,6 +28,11 @@ for i in list_of_dists:
 		for i in revenue:
 			y = i.text
 
+	elif i == 'kancheepuram': #kanchi division and taluks get omitted
+		revenue = content.find(('div'),attrs={'class':'list-text blue-color'})
+		for i in revenue:
+			y = i 
+
 	else:	
 		#sometimes here if..else pudukottai,ramnad,tanjore,vellore
 		revenue = content.findAll(('div'),attrs={'class':'list-text blue-color'})
@@ -36,6 +41,7 @@ for i in list_of_dists:
 	
 	rev_content = [int(x) for x in re.findall('\d+',y)]
 	sorted_rev_content = sorted(rev_content)
+	#print sorted_rev_content
 	if len(sorted_rev_content) == 4:
 		del sorted_rev_content[2]
 	if len(sorted_rev_content) == 1:
@@ -50,37 +56,48 @@ for i in list_of_dists:
 	gen_content = [float(u) for u in re.findall('\d+\d+\.?\d+',zz)]
 	sorted_gen_content = sorted(gen_content)
 	#print sorted_gen_content
+	if len(sorted_gen_content) == 0:
+		sorted_gen_content = [0] #tiruvannamalai area gets omitted
 	n = len(sorted_gen_content)
 	sorted_gen_content = sorted_gen_content[n-1:]
-	'''
-	if len(sorted_gen_content) == 4:
-		sorted_gen_content = sorted_gen_content[3:]
-	elif len(sorted_gen_content) == 5:
-		sorted_gen_content = sorted_gen_content[4:]
-	elif len(sorted_gen_content) == 6:
-		sorted_gen_content = sorted_gen_content[5:]
-	elif len(sorted_gen_content) == 7:
-		sorted_gen_content = sorted_gen_content[6:]
-	elif len(sorted_gen_content) == 8:
-		sorted_gen_content = sorted_gen_content[7:]
-	elif len(sorted_gen_content) == 9:
-		sorted_gen_content = sorted_gen_content[8:]
-	'''
+
 	dist_details = sorted_rev_content + sorted_gen_content
 	root.append(dist_details)
 
 print root
 
+divisions = []
+taluks = []
+villages = []
+population = []
+i = 0
+while i < len(root):
+	x = root[i][0]
+	divisions.append(x)
+	y = root[i][1]
+	taluks.append(y)
+	z = root[i][2]
+	villages.append(z)
+	w = root[i][3]
+	population.append(w)
+	i = i+1
+'''
+print divisions
+print taluks
+print villages
+print population
+
+
 print "\n 1 - Divisions\n 2 - Taluks\n 3 - Villages\n 4 - Area\n 5 - Population"
-
-ss = int(raw_input("enter what to plot:"))
-
-final = [(list_of_dists[0],root[0][ss-1]),(list_of_dists[1],root[1][ss-1])]
 
 x_axis = zip(*final)[0]
 y_axis = zip(*final)[1]
 x_pos = np.arange(len(list_of_dists)) 
+'''
+def plot_graphs(x):
+	index = np.arange(len(list_of_dists))
+	plt.bar(index,x,align='center')
+	plt.xticks(index,list_of_dists,fontsize = 10)
+	plt.show()
 
-plt.bar(x_pos,y_axis)
-plt.show()
-
+plot_graphs(population)
