3102d2d2136abf67d75a8b64f587a993556770c9
aji <46032638+ajigeo@users.noreply.github.com>
2019-03-05 16:10:34 +0530

    Plots must be fixed

--- dist_tn.py
+++ dist_tn.py
@@ -4,17 +4,18 @@ import re
 import numpy as np                                                               
 import matplotlib.pyplot as plt
 import ogr
+import geopandas as gp
 
 list_of_dists = []
 root = []
 
-data = ogr.Open("new_tn.shp",update = 1)
+data = ogr.Open("tamil_districts.shp",update = 1)
 layer = data.GetLayer()
 for i in layer:
 	j =  i.GetField("FIRST_DIST")
 	list_of_dists.append(j)
     
-layer = data.GetLayerByName('new_tn')
+layer = data.GetLayerByName('tamil_districts')
 layer.ResetReading
 
 field_defn = ogr.FieldDefn( "DIVSION", ogr.OFTInteger )
@@ -34,66 +35,71 @@ layer.CreateField(field_defn)
 
 
 for i in list_of_dists:
-	link = urllib2.urlopen('https://'+i+'.nic.in/')
-	source = link.read()
-	content = BeautifulSoup(source)
-
-	try:
-		if i == 'ramanathapuram' or i == 'thanjavur' or i == 'vellore':
-			revenue = content.findAll(('div'),attrs={'class':'list-text green-color'})
-			for i in revenue:
-				y = i.text
-
-		elif i == 'pudukkottai' or i == 'kanniyakumari':
-			revenue = content.findAll(('div'),attrs={'class':'list-text red-color'})
-			for i in revenue:
-				y = i.text
-
-		elif i == 'kancheepuram': #kanchi division and taluks get omitted
-			revenue = content.find(('div'),attrs={'class':'list-text blue-color'})
-			for i in revenue:
-				y = i 
-
-		else:	
-			#sometimes here if..else pudukottai,ramnad,tanjore,vellore
-			revenue = content.findAll(('div'),attrs={'class':'list-text blue-color'})
-			for i in revenue:
-				y = i.text
-		
-		rev_content = [int(x) for x in re.findall('\d+',y)]
-		sorted_rev_content = sorted(rev_content)
-		#print sorted_rev_content
-		if len(sorted_rev_content) == 4:
-			del sorted_rev_content[2]
-		if len(sorted_rev_content) == 1:
-			sorted_rev_content = [0,0] + sorted_rev_content
-
-	except NameError:
-		sorted_rev_content = [0,0,0]		
-			
-	try:	#sometimes here if..else ariyalur,theni,krishn,nellai,trichy,tiruvallur,tiruvannmlai,tirupur,tiruvarur	
-		general = content.findAll(('div'),attrs = {'class':'wpb_column vc_column_container vc_col-sm-4 vc_col-has-fill'})
-		for j in general:
-			yy = j.text
-			zz = yy.replace(',', '')
-
-		gen_content = [float(u) for u in re.findall('\d+\d+\.?\d+',zz)]
-		sorted_gen_content = sorted(gen_content)
-		#print sorted_gen_content
-		if len(sorted_gen_content) == 0:
-			sorted_gen_content = [0] #tiruvannamalai area gets omitted
-		n = len(sorted_gen_content)
-		sorted_gen_content = sorted_gen_content[n-1:]
-
-	except NameError:
-		sorted_gen_content = [0]	
-		
-	dist_details = sorted_rev_content + sorted_gen_content
-	root.append(dist_details)
+    try:
+    	link = urllib2.urlopen('https://'+i+'.nic.in/')
+    	source = link.read()
+    	content = BeautifulSoup(source)
+    
+    	try:
+    		if i == 'ramanathapuram' or i == 'thanjavur' or i == 'vellore':
+    			revenue = content.findAll(('div'),attrs={'class':'list-text green-color'})
+    			for i in revenue:
+    				y = i.text
+    
+    		elif i == 'pudukkottai' or i == 'kanniyakumari':
+    			revenue = content.findAll(('div'),attrs={'class':'list-text red-color'})
+    			for i in revenue:
+    				y = i.text
+    
+    		elif i == 'kancheepuram': #kanchi division and taluks get omitted
+    			revenue = content.find(('div'),attrs={'class':'list-text blue-color'})
+    			for i in revenue:
+    				y = i 
+    
+    		else:	
+    			#sometimes here if..else pudukottai,ramnad,tanjore,vellore
+    			revenue = content.findAll(('div'),attrs={'class':'list-text blue-color'})
+    			for i in revenue:
+    				y = i.text
+    		
+    		rev_content = [int(x) for x in re.findall('\d+',y)]
+    		sorted_rev_content = sorted(rev_content)
+    		#print sorted_rev_content
+    		if len(sorted_rev_content) == 4:
+    			del sorted_rev_content[2]
+    		if len(sorted_rev_content) == 1:
+    			sorted_rev_content = [0,0] + sorted_rev_content
+    
+    	except NameError:
+    		sorted_rev_content = [0,0,0]		
+    			
+    	try:	#sometimes here if..else ariyalur,theni,krishn,nellai,trichy,tiruvallur,tiruvannmlai,tirupur,tiruvarur	
+    		general = content.findAll(('div'),attrs = {'class':'wpb_column vc_column_container vc_col-sm-4 vc_col-has-fill'})
+    		for j in general:
+    			yy = j.text
+    			zz = yy.replace(',', '')
+    
+    		gen_content = [float(u) for u in re.findall('\d+\d+\.?\d+',zz)]
+    		sorted_gen_content = sorted(gen_content)
+    		#print sorted_gen_content
+    		if len(sorted_gen_content) == 0:
+    			sorted_gen_content = [0] #tiruvannamalai area gets omitted
+    		n = len(sorted_gen_content)
+    		sorted_gen_content = sorted_gen_content[n-1:]
+    
+    	except NameError:
+    		sorted_gen_content = [0]	
+    		
+    	dist_details = sorted_rev_content + sorted_gen_content
+    	root.append(dist_details)
+    
+    except:
+        dist_details = [0,0,0,0]
+        root.append(dist_details)
 
 #print root
 
-data = ogr.Open("new_tn.shp",update = 1)
+data = ogr.Open("tamil_districts.shp",update = 1)
 layer = data.GetLayer()
 j = 0
 for i in layer:
@@ -106,7 +112,7 @@ for i in layer:
     i.SetField('POPULATION',root[j][3])
     layer.SetFeature(i)
     j = j+1
-
+data = None
 
 divisions = []
 taluks = []
@@ -123,12 +129,31 @@ for i in range(len(root)):
 	w = root[i][3]
 	population.append(w)
 
-
 def plot_graphs(x):
 	index = np.arange(len(list_of_dists))
 	plt.bar(index,x,align='center')
-	plt.xticks(index,list_of_dists,fontsize = 10)
+	plt.xticks(index,list_of_dists,fontsize = 10,rotation = 90)
 	plt.show()
 
+plt.figure(1,figsize=(12,8))
+plt.subplot2grid((2,2), (0,0))
+plt.title('DIST WISE POPULATION')
 plot_graphs(population)
+
+plt.subplot2grid((2,2), (0,1))
+plt.title('VILLAGES IN TN')
+plot_graphs(villages)
+
+map_content = gp.read_file('tamil_districts.shp')
+
+plt.subplot2grid((2,2), (1,0))
+plt.title('DIST WISE POPULATION')
+map_content.plot('POPULATION',cmap='YlGn')
+
+plt.subplot2grid((2,2), (1,1))
+plt.title('VILLAGES IN TN')
+map_content.plot('VILLAGE',cmap='YlGnBu')
+
+plt.show()
+
 
