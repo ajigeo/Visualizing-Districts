27e8ec95c043636395cbecf649087d8c185e3a7b
aji <46032638+ajigeo@users.noreply.github.com>
2019-03-06 14:28:16 +0530

    Added few comments for understanding

--- dist_tn.py
+++ dist_tn.py
@@ -6,9 +6,10 @@ import matplotlib.pyplot as plt
 import ogr
 import geopandas as gp
 
-list_of_dists = []
-root = []
+list_of_dists = [] #list containing all the district names taken from the shapefile
+root = [] #list of list with the sdistrict name and the 4 attributes - divion, taluk,village,population
 
+#open the shapefile and gets the district name alone from FIRST_DIST column
 data = ogr.Open("tamil_districts.shp",update = 1)
 layer = data.GetLayer()
 for i in layer:
@@ -17,7 +18,7 @@ for i in layer:
     
 layer = data.GetLayerByName('tamil_districts')
 layer.ResetReading
-
+#creates the 4 columns which we want 
 field_defn = ogr.FieldDefn( "DIVSION", ogr.OFTInteger )
 layer.CreateField(field_defn)
 field_defn = ogr.FieldDefn( "TALUK", ogr.OFTInteger )
@@ -27,44 +28,44 @@ layer.CreateField(field_defn)
 field_defn = ogr.FieldDefn( "POPULATION", ogr.OFTInteger )
 layer.CreateField(field_defn)
 
-
+#iterate through all the districts and get the data
 for i in list_of_dists:
     try:
     	link = urllib2.urlopen('https://'+i+'.nic.in/')
     	source = link.read()
     	content = BeautifulSoup(source)
     
-    	try:
+    	try: #revenue contents in green color 
     		if i == 'ramanathapuram' or i == 'thanjavur' or i == 'vellore':
     			revenue = content.findAll(('div'),attrs={'class':'list-text green-color'})
     			for i in revenue:
     				y = i.text
     
-    		elif i == 'pudukkottai' or i == 'kanniyakumari':
+    		elif i == 'pudukkottai' or i == 'kanniyakumari': #revenue contents in pink color
     			revenue = content.findAll(('div'),attrs={'class':'list-text red-color'})
     			for i in revenue:
     				y = i.text
     
-    		elif i == 'kancheepuram': #kanchi division and taluks get omitted
+    		elif i == 'kancheepuram': #kanchi has many contents in blue color so used only find instead of findall
     			revenue = content.find(('div'),attrs={'class':'list-text blue-color'})
     			for i in revenue:
     				y = i 
     
     		else:	
-    			#sometimes here if..else pudukottai,ramnad,tanjore,vellore
+    			#works for most of the districts in this way
     			revenue = content.findAll(('div'),attrs={'class':'list-text blue-color'})
     			for i in revenue:
     				y = i.text
     		
     		rev_content = [int(x) for x in re.findall('\d+',y)]
     		sorted_rev_content = sorted(rev_content)
-    		#print sorted_rev_content
-    		if len(sorted_rev_content) == 4:
+    		#print sorted_rev_content and limit the length of contents to 3
+    		if len(sorted_rev_content) == 4: 
     			del sorted_rev_content[2]
     		if len(sorted_rev_content) == 1:
     			sorted_rev_content = [0,0] + sorted_rev_content
     
-    	except NameError:
+    	except NameError: #for vellore
     		sorted_rev_content = [0,0,0]		
     			
     	try:	#sometimes here if..else ariyalur,theni,krishn,nellai,trichy,tiruvallur,tiruvannmlai,tirupur,tiruvarur	
@@ -87,11 +88,11 @@ for i in list_of_dists:
     	dist_details = sorted_rev_content + sorted_gen_content
     	root.append(dist_details)
     
-    except:
+    except: #for nagapattinam
         dist_details = [0,0,0,0]
         root.append(dist_details)
 
-#print root
+#open the shape file and add the contents districtwise from root
 data = ogr.Open("tamil_districts.shp",update = 1)
 layer = data.GetLayer()
 j = 0
@@ -107,6 +108,7 @@ for i in layer:
     j = j+1
 data = None
 
+#from root take all the attributes and store it in lists for bar plot
 divisions = []
 taluks = []
 villages = []
@@ -122,13 +124,14 @@ for i in range(len(root)):
 	w = root[i][3]
 	population.append(w)
 
+#bar plot
 fig = plt.figure()
 fig.set_size_inches(15,15)
 fig.suptitle('POPULATION')
 index = np.arange(len(list_of_dists))
 plt.bar(index,population,align='center')
 plt.xticks(index,list_of_dists,fontsize = 10,rotation = 90)
-fig.savefig('POP_BAR.png',dpi=1080)
+fig.savefig('POP_BAR.png')
 
 fig = plt.figure()
 fig.set_size_inches(15,15)
@@ -136,9 +139,9 @@ fig.suptitle('VILLAGES')
 index = np.arange(len(list_of_dists))
 plt.bar(index,villages,align='center')
 plt.xticks(index,list_of_dists,fontsize = 10,rotation = 90)
-fig.savefig('VILLAGE_BAR.png',dpi=1080)
+fig.savefig('VILLAGE_BAR.png')
 
-fig,ax = plt.subplots(1, figsize=(25,25))
+#choropleth map using gpd
 map_content = gp.read_file('tamil_districts.shp')
 
 map_content.plot('POPULATION',cmap='YlGn')
